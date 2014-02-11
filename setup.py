a1914af3673ae5062de141f8ce86f8e65c816140
Jay Leadbetter <jay.leadbetter@gmail.com>
2014-02-11 07:23:11 -0700

    [closes #1] Different setup.py configuration depending on install vs. develop

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 import os
+import sys
 from setuptools import setup, find_packages
-
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 
-setup(
-    name='model_ninja',
-    version='0.1',
-    description='Django libraries to facilitate hiding models instead of deleting them.',
-    long_description=open(os.path.join(ROOT, 'README.md')).read(),
-    author='Jay Leadbetter',
-    author_email='codemonkey@jleadbetter.com',
-    url='http://bitbucket.org/kamni/django-disabled-model',
-    license='Affero GPL v3',
-    #packages=find_packages(exclude=['tests', 'tests/*']), # TODO: different excludes for dev versus stable
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=['Django>=1.5'], 
-    tests_require=['django-nose>=1.2'],
-    # TODO: include classifiers
-)
+base_config = {'name': 'model_ninja',
+               'version': '0.1',
+               'description': 'Django libraries to facilitate hiding models instead of deleting them.',
+               'long_description': open(os.path.join(ROOT, 'README.md')).read(),
+               'author': 'J Leadbetter',
+               'author_email': 'codemonkey@jleadbetter.com',
+               'url': 'http://bitbucket.org/kamni/django-disabled-model',
+               'license': 'Affero GPL v3',
+               'include_package_data': True,
+               'zip_safe': False,
+               'install_requires': ['Django>=1.5'],
+               # TODO: include classifiers
+              }
+                        
+if 'develop' in sys.argv:
+    base_config.update({'packages': find_packages(),
+                        'tests_require': ['django-nose>=1.2']})
+else:
+    base_config['packages'] = find_packages(exclude=['model_ninja.tests'])
+
+setup(**base_config)
